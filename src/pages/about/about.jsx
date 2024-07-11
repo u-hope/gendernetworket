e6009282474f42e90bfbf18fc74df9ac9924ea80
Ermias <ermiasa3@gmail.com>
2024-07-11 06:10:22 +0300

    color addition to  About section

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -42,8 +42,8 @@ function About(props) {
   return (
     <div className="about_main">
       <div className="about_top">
-        <div className="about_title">About Us</div>
-        <div className="about_body" style={{textAlign:"center"}}>
+        <div className="about_title" style={{color:"#00859B"}}>About Us</div>
+        <div className="about_body" style={{textAlign:"center",color:"rgba(76, 115, 173, 0.514)"}}>
         Our mission is to enhance visibility and awareness of gender equality issues, amplifying the voices of underrepresented communities. We facilitate collaboration and networking between a diverse range of organizations, NGOs, and advocates to foster synergies and collective action. By streamlining access to essential resources, educational materials, and empowering tools, we empower individuals and strengthen the capacity of gender equality champions. We support grassroots movements in influencing policy and driving systemic change through strategic advocacy efforts. Join our movement as we work together to build a more equitable and inclusive world for all.
         </div>
       </div>
