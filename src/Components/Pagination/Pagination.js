ab2c6b9f974735c575d9bec9d9d341953e2877ae
Ermias <ermiasa3@gmail.com>
2024-07-15 19:06:58 +0300

    added active color on pagination

--- src/Components/Pagination/Pagination.js
+++ src/Components/Pagination/Pagination.js
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import './Pagination.css';
 
-const Pagination = ({totalRecords, recordsPerPage,setCurrentPage}) => {
+const Pagination = ({totalRecords, recordsPerPage, setCurrentPage}) => {
+    const [activeButton, setActiveButton] = useState(1);
 
     let pages = [];
     for(let i = 1; i<=Math.ceil(totalRecords/recordsPerPage); i++){
         pages.push(i);
     }
+
+    const handlePageClick = (page) => {
+        setCurrentPage(page);
+        setActiveButton(page);
+    }
+
     return(
         <div className="pagination">
             {
                 pages.map((page, index) =>{
-                    return <button key={index} className="paginationButton" onClick={()=>setCurrentPage(page)}>{page}</button>
+                    return <button key={index} className={`paginationButton ${activeButton === page ? 'active' : ''}`} onClick={() => handlePageClick(page)}>{page}</button>
                 })
             }
         </div>
